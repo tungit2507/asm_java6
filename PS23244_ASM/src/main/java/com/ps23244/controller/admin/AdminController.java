fab604542903f37e6f8efdafe727014485c48044
TrieuBao <baotrieu27122003@gmail.com>
2023-07-17 00:35:39 +0700

    Run

--- PS23244_ASM/src/main/java/com/ps23244/controller/admin/AdminController.java
+++ PS23244_ASM/src/main/java/com/ps23244/controller/admin/AdminController.java
@@ -1,4 +1,4 @@
-	package com.ps23244.controller.admin;
+package com.ps23244.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,5 +10,5 @@ public class AdminController {
 	public String index() {
 		return "admin/index";
 	}
-	
+
 }
--- PS23244_ASM/src/main/webapp/views/admin/menu/footer.jsp
+++
@@ -1,6 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<footer class="main-footer text-center">
-    <strong>Copyright &copy; 2023 .</strong>
-    vinhhtps23244@fpt.edu.vn
-  </footer>
